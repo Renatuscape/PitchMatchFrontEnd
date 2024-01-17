2b52689b7ec28857d2c549e7138862dfbb44eeea
Renatuscape <silarcta@gmail.com>
2024-01-17 12:46:36 +0100

    Initial

---
+++ PitchMatch/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>PitchMatch</h1>
+      <p className="read-the-docs">
+        Product by FixedCreations
+      </p>
+    </>
+  )
+}
+
+export default App
