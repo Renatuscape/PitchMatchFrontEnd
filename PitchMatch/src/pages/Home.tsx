ae322b038fac65a13298bf5739f396de4442f28b
Renatuscape <silarcta@gmail.com>
2024-01-18 15:54:47 +0100

    Added image box

--- PitchMatch/src/pages/Home.tsx
+++ PitchMatch/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 export function Home(){
     return<div className="page-background" style={{minHeight:'75vh'}}>
-        <img className="home-picture" src='https://ncas.ac.uk/app/uploads/2022/08/DCMEX-Cloud-Formation-1280px.jpg'/>
+        <div className="home-picture">hi</div>
     </div>
 }
