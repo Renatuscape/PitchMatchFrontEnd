43f109981f785065cea3f98c93160e4104b25ff5
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-01-18 14:44:59 +0100

    installed router, added some router elemets

--- PitchMatch/src/App.tsx
+++ PitchMatch/src/App.tsx
@@ -1,18 +1,26 @@
 import './App.css'
 import Button from '@mui/material/Button';
 import ResponsiveAppBar from './components/ResponsiveAppBar';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
 
 function App() {
 
   return (
     <>
-    <ResponsiveAppBar/>
-      <p className="read-the-docs">
-        Product by FixedCreations
-      </p>
-
-      <Button variant="contained">PitchMatch</Button>
-      <div></div>
+        <BrowserRouter>
+        <ResponsiveAppBar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+  
+        </Routes>
+        <p className="read-the-docs">
+          Product by FixedCreations
+        </p>
+        </BrowserRouter>
     </>
   )
 }
---
+++ PitchMatch/src/pages/About.tsx
@@ -0,0 +1,5 @@
+export function About(){
+    return<>
+    <h1>hei</h1>
+    </>
+}
