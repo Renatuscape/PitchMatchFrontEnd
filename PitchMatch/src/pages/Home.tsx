9f872f42c3747210f477fb544e3ed2edeb8420fd
Renatuscape <silarcta@gmail.com>
2024-01-18 16:17:31 +0100

    Home page updated with floating text in grid

--- PitchMatch/src/pages/Home.tsx
+++ PitchMatch/src/pages/Home.tsx
@@ -1,5 +1,9 @@
 export function Home(){
     return<div className="page-background" style={{minHeight:'75vh'}}>
-        <div className="home-picture">hi</div>
+        <div className="home-picture">
+            <div className="goal-float">2,300<p>Projects funded</p></div>
+            <div className="goal-float" id="goal-float-main">4 MIL<p>In annual yields</p></div>
+            <div className="goal-float">6,500<p>Local jobs created</p></div>
+        </div>
     </div>
 }
