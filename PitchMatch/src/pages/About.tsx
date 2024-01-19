7517e4579ece5469c0f777e1823caaacf9b5435b
Renatuscape <silarcta@gmail.com>
2024-01-19 19:46:25 +0100

    Margin adjustment

--- PitchMatch/src/pages/About.tsx
+++ PitchMatch/src/pages/About.tsx
@@ -2,8 +2,8 @@ import { Container } from "@mui/material";
 
 export function About() {
     return <div className='page-background'>
-        <Container maxWidth='md' sx={{ backgroundColor: 'white', padding: 2, display: 'flex', alignItems: 'flex-end' }}>
-            <div>
+        <Container maxWidth='md' sx={{ backgroundColor: 'white', padding: 2, display: 'flex', alignItems: 'flex-end'}}>
+            <div style={{marginBottom: 15}}>
                 <h2>What is PitchMatch?</h2>
                 <p>
                     PitchMatch is a platform that connects early startups with investors.
