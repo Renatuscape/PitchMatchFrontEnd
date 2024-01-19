c54add8c360bdea9f8e6a15eddd23770ad7c2674
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-01-19 20:34:39 +0100

    trying to fix a css problemin createPitch

--- PitchMatch/src/components/CreatePitchComponent.tsx
+++ PitchMatch/src/components/CreatePitchComponent.tsx
@@ -193,6 +193,5 @@ export default function CreatePitchComponent(props: CreatePitchFormProps) {
 const style1 = {
   marginTop: "0 35px",
   marginBottom: "35px",
-  borderRadius: "10px",
   boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
 };
