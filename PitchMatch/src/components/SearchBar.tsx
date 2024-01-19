73821fbc14df848b4c1b5b052cd575a9263bf204
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-01-19 13:28:13 +0100

    added searching bar comp logic

--- PitchMatch/src/components/SearchBar.tsx
+++ PitchMatch/src/components/SearchBar.tsx
@@ -1,14 +1,35 @@
 import { Container, IconButton, InputAdornment, Paper, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import { useEffect, useState } from "react";
+
+type UserSearchProps={
+    id: string;
+    name: string;
+    email: string;
+    location: string;
+    profilePictureUrl: string;
+  }
 
 export function SearchBar(){
-     
+     const[users,setUsers]=useState<UserSearchProps[]>([]);
   
+    useEffect(() => {
+    getAllUsersAsync()
+      .then(user => setUsers(user))
+    }, []);
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const searchValue = event.target.value;
+        const filteredUsers = users.filter((user) => {
+            return user.name.toLowerCase().includes(searchValue.toLowerCase());
+        });
+        setUsers(filteredUsers);
+    }
     return<>
     <Container style={{marginTop:"15px"}}>
         <Paper elevation={3} style={{ padding: '10px', display: 'flex', alignItems: 'center' }}>
             <TextField
                 fullWidth
+                onChange={handleSearch}
                 variant="outlined"
                 placeholder="Search..."
                 size="small"
@@ -18,11 +39,28 @@ export function SearchBar(){
                             <IconButton>
                                 <SearchIcon />
                             </IconButton>
-                        </InputAdornment>
-                    ),
-                }}
-            />
+                        </InputAdornment>),}}/>
         </Paper>
+            <div>
+                {users.map((user:UserSearchProps) =>  <UserSearchComp key={user.id} {...user} />)}
+            </div>
         </Container>
     </>
+}
+
+export function UserSearchComp(user:UserSearchProps){
+return<>
+<div key={user.id}>
+                        <h3>{user.name}</h3>
+                        <p>{user.email}</p>
+                        <p>{user.location}</p>
+                        <img src={user.profilePictureUrl} alt="profile picture" />
+                    </div>
+</>
+}
+
+export async function getAllUsersAsync(){
+const res = await fetch("https://pitchmatch.azurewebsites.net/user")
+const resObject = await res.json();
+return resObject;
 }
