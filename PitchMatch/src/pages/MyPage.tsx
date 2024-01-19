93c9a70421aff6af1d35c99561ff5ff7801d92a5
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-01-19 17:05:12 +0100

    fixed bug

--- PitchMatch/src/pages/MyPage.tsx
+++ PitchMatch/src/pages/MyPage.tsx
@@ -1,13 +1,12 @@
+import { useState } from "react";
 import { UserPageCard1, UserPageCard2, UserPageCard3 } from "../components/UserPageCard";
+import { useParams } from "react-router-dom";
 
 export function MyPage() {
-    return <>
+
+   return <>
     <UserPageCard1/>
-    <UserPageCard2/>
-    <UserPageCard3/>
     </>
-       
-    
 }
 
 
