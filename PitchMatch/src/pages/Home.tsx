1ecc18e03783971c94138b6b1f173e2ced85f115
Renatuscape <silarcta@gmail.com>
2024-01-19 10:08:30 +0100

    Added pitch card to test

--- PitchMatch/src/pages/Home.tsx
+++ PitchMatch/src/pages/Home.tsx
@@ -1,9 +1,14 @@
+import { PitchCard } from "../components/PitchCard";
+
 export function Home(){
     return<div className="page-background" style={{minHeight:'75vh'}}>
         <div className="home-picture">
             <div className="goal-float">2,300<p>Projects funded</p></div>
             <div className="goal-float" id="goal-float-main">4 MIL<p>In annual yields</p></div>
             <div className="goal-float">6,500<p>Local jobs created</p></div>
+        </div>
+        <div>
+            <PitchCard title="Test" content="Test test test"/>
         </div>
     </div>
 }
