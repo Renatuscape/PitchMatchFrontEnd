13c2fe2e2c11151ae85f1e0baa30c71c0dd12c29
Diamondduke <reza.sarmast@brightstraining.com>
2024-01-21 16:01:25 +0100

    Pitch page and component added

---
+++ PitchMatch/src/components/PitchPageComponent.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Container, Card, CardHeader, CardContent, CardMedia, CardActions, Typography, Button, Divider, Grid, Box } from "@mui/material";
+
+export type PitchPageProps = {
+  title: string;
+  category: string;
+  creator: string;
+  goals: string;
+  investors: number;
+  funding: string;
+  risk: number;
+  projectedYield: string;
+  description: string;
+  imageUrl: string;
+};
+
+// This is the component that displays the project details
+export function PitchPageComponent(props: PitchPageProps) {
+    return (
+        <Container maxWidth="sm">
+          <Card sx={{ position: 'relative' }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={props.imageUrl}
+              alt="Background"
+              sx={{ opacity: 0.5 }} // Makes the image more subtle
+            />
+            <Box sx={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, padding: 2 }}>
+              <Typography gutterBottom variant="h5" component="div" color="primary.contrastText">
+                {props.title}
+              </Typography>
+              <Typography variant="body2" color="primary.contrastText">
+                {props.category}
+              </Typography>
+              <CardActions>
+                <Button variant="contained" color="secondary">
+                  Interested
+                </Button>
+              </CardActions>
+              <Divider sx={{ bgcolor: 'primary.contrastText' }} />
+              <CardContent>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <Typography variant="subtitle1">Creator:</Typography>
+                    <Typography>{props.creator}</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="subtitle1">Goals:</Typography>
+                    <Typography>{props.goals}</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="subtitle1">Investors:</Typography>
+                    <Typography>{props.investors}</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="subtitle1">Funding:</Typography>
+                    <Typography>{props.funding}</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="subtitle1">Risk:</Typography>
+                    <Typography>{props.risk}</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="subtitle1">Projected Annual Yield:</Typography>
+                    <Typography>{props.projectedYield}</Typography>
+                  </Grid>
+                </Grid>
+                <Typography variant="body1" sx={{ marginTop: 2 }}>
+                  {props.description}
+                </Typography>
+              </CardContent>
+            </Box>
+          </Card>
+        </Container>
+      );
+    }
+
+
+export const style1 = {
+  margin: "0 35px",
+  boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+};
--- PitchMatch/src/pages/PitchPage.tsx
+++ PitchMatch/src/pages/PitchPage.tsx
@@ -1,5 +1,33 @@
- export function PitchPage(){
-    return <>
-    <h1>PitchPage</h1>
-    </>
- }
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { PitchPageComponent } from '../components/PitchPageComponent';
+import { PitchPageProps } from '../components/PitchPageComponent';
+
+export type UserParamsType = {
+   id: string
+}
+
+export async function getUser(id:number):Promise<PitchPageProps>{
+   const res = await fetch("https://pitchmatch.azurewebsites.net/pitch/"+id)
+   const resObject = await res.json();
+   return resObject;
+}
+
+export function PitchPage() {
+  const [project, setPitch] = useState<PitchPageProps>();
+  const {id} = useParams<keyof UserParamsType>() as UserParamsType;
+
+  useEffect(() => {
+    getUser(parseInt(id)).then((res) => setPitch(res));
+  }, [id]);
+
+  if (!project) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className='page-background'>
+      <PitchPageComponent {...project} />
+    </div>
+  );
+}
