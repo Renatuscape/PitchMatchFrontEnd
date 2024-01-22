c29680c4a5d3e3e164402675f7a079f652bde2ef
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-01-22 13:12:22 +0100

    Added visibility in passwords in create user and log in

--- PitchMatch/src/pages/LogIn.tsx
+++ PitchMatch/src/pages/LogIn.tsx
@@ -2,7 +2,7 @@ import { Create, Height } from "@mui/icons-material";
 import { CreateUserCard } from "../components/CreateUserCard";
 import { Container, Grid } from "@mui/material";
 import { LogInCard } from "../components/LogInCard";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { LogInType } from "../components/types";
 type LoginProps={
@@ -12,6 +12,7 @@ type LoginProps={
 export function LogIn({LoginFunctionality}:LoginProps) {
         const[email,setEmail]=React.useState<string>("")
         const[password,setPassword]=React.useState<string>("")
+        const [showPassword, setShowPassword] = useState(false);
         const navigate = useNavigate();
 
 const handlerSubmit=async(e:React.MouseEvent<HTMLButtonElement>)=>{
@@ -20,13 +21,16 @@ const handlerSubmit=async(e:React.MouseEvent<HTMLButtonElement>)=>{
         await LoginFunctionality(userInfo).then(response=> navigate("/"))
         }
 
+ const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
      
 return<>
 <div className='page-background' style={{height:"700px"}}>
 <Container>
 <Grid container >
         
-         <Grid item xs={6}> <LogInCard onSubmit={handlerSubmit} onChangeEmail={setEmail} onChangePassword={setPassword} email={email} password={password}/> </Grid>
+         <Grid item xs={6}> <LogInCard onSubmit={handlerSubmit} onChangeEmail={setEmail} onChangePassword={setPassword} email={email} password={password} passwordVisibility={showPassword} onTogglePasswordVisibility={handleTogglePasswordVisibility}/> </Grid>
          <Grid item xs={6}> <CreateUserCard/> </Grid>
 </Grid>
 </Container>
