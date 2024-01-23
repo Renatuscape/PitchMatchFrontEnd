48466c6c656378a34b1436e1516eedb936950d57
Renatuscape <silarcta@gmail.com>
2024-01-23 14:45:51 +0100

    Added user and personaldata types

--- PitchMatch/src/components/types.tsx
+++ PitchMatch/src/components/types.tsx
@@ -16,6 +16,30 @@ export type Pitch = {
     email: string;
     password: string;
   }
+
+  export type User = {
+    id:number;
+    name:string;
+    email: string;
+    bio: string;
+    contact:string;
+    soMe:string;
+    imgUrl:string;
+    cvUrl:string;
+    rating:number;
+    portfolio:Pitch[];
+  }
+
+  export type PersonalData = {
+    id:number;
+    phoneNumber:string;
+    personNr:string;
+    address:string;
+    latitude:number;
+    longitude:number;
+    isVerified:boolean;
+    userId:number;
+  }
   
 export type TokenAndId={
     accessToken:string,
