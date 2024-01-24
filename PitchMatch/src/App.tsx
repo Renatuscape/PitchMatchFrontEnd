b0ce53e632ee34ebad18d42763b1aa49d6231b72
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-01-24 10:03:18 +0100

    fix bug

--- PitchMatch/src/App.tsx
+++ PitchMatch/src/App.tsx
@@ -84,7 +84,7 @@ function App() {
                                               }
                                                 />
           <Route path="/edituser" element={ <ProtectedRoute>
-                                                    <EditPitch/>
+                                                    <EditUser/>
                                                 </ProtectedRoute>
                                               }
                                                 />
