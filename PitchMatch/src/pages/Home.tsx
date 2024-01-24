d45dbc211aeb2a2e1844dd03ab8e4ef4b7b86e85
Renatuscape <silarcta@gmail.com>
2024-01-24 17:21:16 +0100

    Fixed homepage layout

--- PitchMatch/src/pages/Home.tsx
+++ PitchMatch/src/pages/Home.tsx
@@ -7,14 +7,14 @@ import { useAuth } from "../App";
 import { DynamicCard } from "../components/DynamicCard";
 
 
-export function Home(){
-  const{token}=useAuth();
+export function Home() {
+  const { token } = useAuth();
   const isLoggedIn: boolean = !!token?.accessToken;
   const userId: number = token?.userId ?? 0;
-    const[pitches,setPitches]=useState<Pitch[]>([])
-   
+  const [pitches, setPitches] = useState<Pitch[]>([])
 
-   useEffect(() => {
+
+  useEffect(() => {
     const fetchData = async () => {
       try {
         const pitchesData = await getPitchesAsync();
@@ -27,31 +27,35 @@ export function Home(){
     fetchData();
   }, []);
 
-    return <div className="page-background" style={{minHeight:'75vh'}}>
-        <div className="home-picture">
-            <div className="goal-float">2,300<p>Projects funded</p></div>
-            <div className="goal-float" id="goal-float-main">4 MIL<p>In annual yields</p></div>
-            <div className="goal-float">6,500<p>Local jobs created</p></div>
-        </div>
-        <Box style={{width: '100vw', marginLeft: '55px',display: 'grid', gap: 10, gridTemplateColumns: '1fr 1fr 1fr', gridTemplateRows: 'auto'}}>
-            {pitches.map((pitch, index) => (
-  isLoggedIn ? (
-    <Link key={index + pitch.id} to={`/pitch/${pitch.id}`} style={{ textDecoration: 'none' }}>
-      <DynamicCard key={index + pitch.id} pitch={pitch}/>
-    </Link>
-    
-  ) : (
-    <Link key={index + pitch.id} to="/login" style={{ textDecoration: 'none' }}>
-      <DynamicCard key={index + pitch.id} pitch={pitch}/>
-    </Link>
-  )
-))}
-        </Box>
+  return <div className="page-background" style={{ height: 'auto', display: 'flex' }}>
+    <div className="home-picture" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }}>
+      <div style={{ display: 'flex', gap: 50, justifyContent: 'space-evenly', marginTop: 75, marginBottom: '20vw'}}>
+        <div className="goal-float">2,300<p>Projects funded</p></div>
+        <div className="goal-float" id="goal-float-main">4 MIL<p>In annual yields</p></div>
+        <div className="goal-float">6,500<p>Local jobs created</p></div>
+      </div>
+      <div style={{width: '100vw', display: 'flex', justifyContent: 'center'}}>
+      <Box style={{display: 'grid', gap: 20, paddingBottom: 30, gridTemplateColumns: '1fr 1fr 1fr', gridTemplateRows: 'auto' }}>
+        {pitches.map((pitch, index) => (
+          isLoggedIn ? (
+            <Link key={index + pitch.id} to={`/pitch/${pitch.id}`} style={{ textDecoration: 'none' }}>
+              <DynamicCard key={index + pitch.id} pitch={pitch} />
+            </Link>
+
+          ) : (
+            <Link key={index + pitch.id} to="/login" style={{ textDecoration: 'none' }}>
+              <DynamicCard key={index + pitch.id} pitch={pitch} />
+            </Link>
+          )
+        ))}
+      </Box>
+      </div>
     </div>
+  </div>
 }
 
-export async function getPitchesAsync():Promise<Pitch[]>{
-   const res = await fetch("https://pitchmatch.azurewebsites.net/Home")
-   const resObject = await res.json();
-   return resObject;
+export async function getPitchesAsync(): Promise<Pitch[]> {
+  const res = await fetch("https://pitchmatch.azurewebsites.net/Home")
+  const resObject = await res.json();
+  return resObject;
 }
