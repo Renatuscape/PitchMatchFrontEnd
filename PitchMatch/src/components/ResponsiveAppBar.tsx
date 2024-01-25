a3816db1341f181611bc4aab3cf3393e973087c1
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-01-25 10:35:21 +0100

    Add useNavigate hook to handle navigation to login page

--- PitchMatch/src/components/ResponsiveAppBar.tsx
+++ PitchMatch/src/components/ResponsiveAppBar.tsx
@@ -1,5 +1,5 @@
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { LoggedInIcon } from '../Context/contextPage';
 import { useEffect, useState } from 'react';
 import { useAuth } from '../App';
@@ -7,12 +7,17 @@ import { useAuth } from '../App';
 export function ResponsiveAppBar() {
   const { token } = useAuth();
   const isLoggedIn: boolean = !!token?.accessToken;
+  const navigate = useNavigate();
 
   useEffect(() => {
     const res = localStorage.getItem('logInStatus') === 'true' ? true : false;
    
   }, [token]);
   
+  const handleOnClick=()=>{
+    navigate('/login');
+  }
+
   return (
     <div className="header-container">
       <Link to="/search">
@@ -37,9 +42,9 @@ export function ResponsiveAppBar() {
         </Button>
       </Link>
      
-        {isLoggedIn ? (<div  style={{marginTop: 'auto'}}><LoggedInIcon/></div>) : (<Link to='/login'><Button sx={{ my: 2, color: 'black', display: 'block' , "&:focus":{outline: "none",}}}>
+        {isLoggedIn ? (<div  style={{marginTop: 'auto'}}><LoggedInIcon/></div>) : (<Button onClick={handleOnClick} sx={{ my: 2, color: 'black', display: 'block' , "&:focus":{outline: "none",}}}>
         Log In
-        </Button></Link>)}
+        </Button>)}
       
     </div>
   );
